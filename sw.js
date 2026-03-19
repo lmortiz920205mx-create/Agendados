39ab2ed2af194c3b0fc6822f69c068c2f0f78a3f
lmortiz920205mx-create <lmortiz920205mx@gmail.com>
2026-03-19 02:36:40 -0600

    V.1.1

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = "platino-v-final";
+const resources = ["index.html", "manifest.json"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(resources)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
