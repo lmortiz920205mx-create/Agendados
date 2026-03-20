f033cf13d4ca2d756ac45f1bb2a7e228ed2d2297
lmortiz920205mx-create <lmortiz920205mx@gmail.com>
2026-03-20 00:18:56 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,30 @@
-const CACHE_NAME = "platino-v2.2";
-const resources = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icono.png"
+const CACHE = "taxi-platino-v2.3";
+
+const ASSETS = [
+ "./",
+ "./index.html",
+ "./manifest.json",
+ "./alerta.mp3"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(resources))
-  );
+self.addEventListener("install", e=>{
+ e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS))
+ );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("activate", e=>{
+ e.waitUntil(
+  caches.keys().then(keys=>
+   Promise.all(keys.map(k=>{
+    if(k!==CACHE) return caches.delete(k);
+   }))
+  )
+ );
+});
+
+self.addEventListener("fetch", e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
 });
