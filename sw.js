cf0225adc65e0e7c990f24382fac7e3a53d767a8
lmortiz920205mx-create <lmortiz920205mx@gmail.com>
2026-03-22 00:01:40 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taxi-platino-v1';
+const CACHE_NAME = 'taxi-platino-v1.1';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
